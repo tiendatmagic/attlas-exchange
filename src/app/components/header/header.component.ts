1ac35a639e0154aeccf3d4554f5b31012a0224ce
tiendatmagic <tiendatmagic8@gmail.com>
2023-02-07 23:26:31 +0700

    update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,25 +8,8 @@ import { Router, NavigationStart, Event as NavigationEvent } from '@angular/rout
 export class HeaderComponent {
   logo: string = 'assets/images/logo-light-horizontal.png';
   showMenu: boolean = false;
-  event$;
-  public homePage = false;
 
-  constructor(private router: Router) {
-
-    this.event$
-      = this.router.events
-        .subscribe(
-          (event: NavigationEvent) => {
-            if (event instanceof NavigationStart) {
-              if (event.url == "/") {
-                this.homePage = true;
-              }
-              else {
-                this.homePage = false;
-              }
-            }
-          });
-  }
+  constructor(private router: Router) { }
   openMenu() {
     this.showMenu = !this.showMenu;
   }
