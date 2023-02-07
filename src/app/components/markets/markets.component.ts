56c1c6baedf81a871ddf989001f66c96cb41f4b9
tiendatmagic <tiendatmagic8@gmail.com>
2023-02-08 01:46:36 +0700

    update

--- src/app/components/markets/markets.component.ts
+++ src/app/components/markets/markets.component.ts
@@ -12,13 +12,30 @@ export class MarketsComponent {
   any = [];
   isLoading: boolean = true;
   constructor(private http: HttpClient, private data: DataService) { }
+  loadData() {
+    var run;
+    clearInterval(run);
+    this.data.getPrice().subscribe((res: any) => {
+      this.coinLists = (res['data']);
+      console.log(this.coinLists);
+      localStorage.setItem("coinLists", JSON.stringify(this.coinLists));
+      this.isLoading = false;
+      run = setInterval(() => {
+        this.loadData();
+      }, 40000)
+    }
+    );
+  }
 
   ngOnInit() {
-    {
+    if (localStorage.getItem("coinLists")) {
       for (var i = 0; i < 10; i++) {
         this.coinLists.push(JSON.parse(`${localStorage.getItem("coinLists")}`)[i]);
       }
       this.isLoading = false;
+    }
+    else {
+      this.loadData();
     }
   }
 }
