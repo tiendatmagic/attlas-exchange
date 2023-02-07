6583d44c4af62476dfc76ab808aa198fd694e62c
tiendatmagic <tiendatmagic8@gmail.com>
2023-02-07 21:58:45 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'posts': '0px 8px 24px #ededed',
+      }
+    },
   },
   plugins: [],
 }
