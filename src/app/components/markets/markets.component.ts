546194b2a24b87d55489e76c5db42de97165a90a
tiendatmagic <tiendatmagic8@gmail.com>
2023-02-09 21:35:45 +0700

    update

--- src/app/components/markets/markets.component.ts
+++ src/app/components/markets/markets.component.ts
@@ -17,11 +17,9 @@ export class MarketsComponent {
 
     run = setInterval(() => {
       this.coinLists.length = 0;
-      this.data.getPrice().subscribe((res: any) => {
-        this.coinLists = (res['data']);
-        localStorage.setItem("coinLists", JSON.stringify(this.coinLists));
-        this.isLoading = false;
-      });
+      for (var i = 0; i < 10; i++) {
+        this.coinLists.push(JSON.parse(`${localStorage.getItem("coinLists")}`)[i]);
+      }
     }, 40000)
   }
 
@@ -29,16 +27,11 @@ export class MarketsComponent {
     if (localStorage.getItem("coinLists")) {
       var run;
       clearInterval(run);
-      run = setInterval(() => {
-        this.coinLists.length = 0;
-        for (var i = 0; i < 10; i++) {
-          this.coinLists.push(JSON.parse(`${localStorage.getItem("coinLists")}`)[i]);
-        }
-        this.isLoading = false;
-      }, 40000)
+      this.isLoading = false;
+      for (var i = 0; i < 10; i++) {
+        this.coinLists.push(JSON.parse(`${localStorage.getItem("coinLists")}`)[i]);
+      }
     }
-    else {
-      this.loadData();
-    }
+    this.loadData();
   }
 }
